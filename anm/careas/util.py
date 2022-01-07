44f1fb929c76b4434cea6575d188e91811cd9294
André <eusoubrasileiro@gmail.com>
2022-01-07 10:43:53 -0300

    util.py better `ContaPrazo` with `strftime`

--- anm/careas/util.py
+++ anm/careas/util.py
@@ -2,13 +2,13 @@ import datetime
 import sys
 from dateutil.relativedelta import relativedelta
 
-def ContaPrazo(dou_ano=1993, dou_mes=12, dou_dia=21, prazo_dias=30, prazo_meses=0):
+def ContaPrazo(dou_dia=21, dou_mes=12, dou_ano=1993, prazo_dias=30, prazo_meses=0):
     """
     Contagem de Prazo
     PARECER/PROGE Nº 173, de 2008
     item 24. (a,b,c) Conclusão definem a regra
 
-    * dataDOU: tuple(ano, mes, dia)
+    * dataDOU: tuple(dia, mes, ano)
         data da publicacao em DOU
     """
     start = datetime.datetime(dou_ano, dou_mes, dou_dia)
@@ -20,10 +20,10 @@ def ContaPrazo(dou_ano=1993, dou_mes=12, dou_dia=21, prazo_dias=30, prazo_meses=
     # o prazo começa a contar no dia seguinte ao da publicaçao
     # desde que exista expediente nesse dia (ou seja dia útil)
     # caso nao seja a contagem começa a contar do próximo dia com expediente (ou dia útil)
-    print('Publicado    :', start, WeekDayr[start.weekday()], file=sys.stdout)
+    print('Publicado    :', start.strftime(r"%d/%m/%y"), WeekDayr[start.weekday()], file=sys.stdout)
     if start.weekday()+1 == WeekDay['Saturday']: # contagem começaria no sábado
         start = start + relativedelta(days=2) # ínicio é domingo para prazo começar na segunda
-    print('Ínicio       :', start, WeekDayr[start.weekday()], file=sys.stdout)
+    print('Ínicio       :', start.strftime(r"%d/%m/%y"), WeekDayr[start.weekday()], file=sys.stdout)
     # isso deve-se ao fato da data final ser incluída na contagem
     # e.g dia 25/10/2010 sexta-feira publicado - prazo 2 dias
     # contagem inicia no dia seguinte, entretanto, esse é sábado logo
@@ -31,10 +31,10 @@ def ContaPrazo(dou_ano=1993, dou_mes=12, dou_dia=21, prazo_dias=30, prazo_meses=
     # 27/10/2010 domingo, e soma-se 2 dias para chegar a data final (incluída)
     # 30/10/2010 terça-feira
     deadline = start + time
-    print('Prazo final  :', deadline, WeekDayr[deadline.weekday()], file=sys.stdout)
+    print('Prazo final  :', deadline.strftime(r"%d/%m/%y"), WeekDayr[deadline.weekday()], file=sys.stdout)
     # último dia do prazo DEVE ser dia de expediente (ou útil)
     if deadline.weekday() == WeekDay['Saturday']:
         deadline += relativedelta(days=2)
     if deadline.weekday() == WeekDay['Sunday']:
         deadline += relativedelta(days=1)
-    print('Prazo final  :', deadline,  WeekDayr[deadline.weekday()], file=sys.stdout)
+    print('Prazo final  :', deadline.strftime(r"%d/%m/%y"),  WeekDayr[deadline.weekday()], file=sys.stdout)
