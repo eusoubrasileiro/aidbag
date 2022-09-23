86f58ddeb2574c4b6deca7c73dc4e6121410d3d5
eusoubrasileiro <eusoubrasileiro@gmail.com>
2022-09-23 16:30:47 -0300

    fix json.py serialize any other object to  '' - any other object except datetime.datetime will be ''

--- web/json.py
+++ web/json.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import sys 
 
 def datetime_to_json(obj):
     """json.dumps default function to use to json from datetime conversion"""
     if isinstance(obj, datetime):
         return { '_isoformat': obj.isoformat() }
-    raise TypeError('...')
+    else:
+        print(f"object of type {type(obj)} converted to '' in JSON file", file=sys.stderr) 
+        return ''        
 
 def json_to_datetime(obj):
     """json.loads object_hook function from json to datetime conversion"""
