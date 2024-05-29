475138eccce525458e56913651546be435627dfa
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-05-29 15:14:59 -0300

    prework saving estudo status to database for react-usage

--- anm/careas/workflows/prework.py
+++ anm/careas/workflows/prework.py
@@ -33,24 +33,30 @@ def BatchPreAnalyses(wpage : wPageNtlm, processos: list[scm.pud],
     succeed_NUPs = [] # suceed 
     failed_NUPS = [] # failed
     for processo in tqdm.tqdm(processos):        
-        processo = processo.str
+        processo = processo.str        
         try:            
             if estudo is ESTUDO_TYPE.INTERFERENCIA:
                 _ = estudos.Interferencia.make(wpage, processo, verbose=verbose, overwrite=overwrite)   
-                proc = scm.ProcessManager[processo].dados
+                proc = scm.ProcessManager[processo]
             elif estudo is ESTUDO_TYPE.OPCAO:
                 _ = scm.ProcessManager.GetorCreate(processo, wpage, task=scm.SCM_SEARCH.BASICOS_POLIGONAL, verbose=verbose)
-                proc = scm.ProcessManager[processo].dados     
+                proc = scm.ProcessManager[processo]     
                 with Sei(kwargs['user'], kwargs['passwd'], headless=True) as seid:
                     psei = Processo.fromSei(seid, proc['NUP'])
-                    psei.download_latest_documents(10)                
-        except DownloadInterferenciaFailed as e:                                             
-            failed_NUPS.append((scm.ProcessManager[processo].dados['NUP'], str(e)))            
+                    psei.download_latest_documents(10) # autocreate processfolder                                  
+        except DownloadInterferenciaFailed as e:
+            dados = proc.dados
+            dados['prework'] = {'status' : 'error', 'error' : str(e)} # save for use on workapp
+            scm.ProcessManager[processo].update(dados)
+            failed_NUPS.append((dados['NUP'], str(e))) 
         except Exception as e:
-            failed_NUPS.append((scm.ProcessManager[processo].dados['NUP'],
+            failed_NUPS.append((proc['NUP'],
                 f"Process {processo} Exception: {traceback.format_exc()}"))            
         else:
-            succeed_NUPs.append(proc['NUP'])  
+            dados = proc.dados
+            dados['prework'] = { 'status' : 'ok' } # save for use on workapp
+            scm.ProcessManager[processo].update(dados)
+            succeed_NUPs.append(proc['NUP']) 
     # print all NUPS
     print('SEI NUPs sucess:')
     for nup in succeed_NUPs:
